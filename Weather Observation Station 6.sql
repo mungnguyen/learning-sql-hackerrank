0b411eca28696839ffcbe06a060085b02ed3ec89
MungNT <admin@admins-MacBook-Pro.local>
2024-02-21 22:41:33 +0700

    Day 01 - 21/02

---
+++ Weather Observation Station 6.sql
@@ -0,0 +1,10 @@
+/*
+Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
+*/
+SELECT DISTINCT(CITY) FROM STATION 
+WHERE 
+    CITY LIKE 'a%' OR CITY LIKE 'A%'
+    OR CITY LIKE 'e%' OR CITY LIKE 'E%'
+    OR CITY LIKE 'i%' OR CITY LIKE 'I%'
+    OR CITY LIKE 'o%' OR CITY LIKE 'O%'
+    OR CITY LIKE 'u%' OR CITY LIKE 'U%';
